dd415ad4ec612c149e7b7e08eea55fc0af599d79
Jeferson Naranjo <jeferson.david.naranjo.barrera@gmail.com>
2025-11-09 16:40:44 -0500

    desarrollo de aplicacion backend post - pruebas error FK

--- Data/CasinoDbContext.cs
+++ Data/CasinoDbContext.cs
@@ -12,10 +12,5 @@ public class CasinoDbContext : DbContext
         modelBuilder.Entity<Ruleta>().ToTable("Ruletas").HasKey(r => r.IdRuleta);
         modelBuilder.Entity<Apuesta>().ToTable("Apuestas").HasKey(a => a.IdApuesta);
 
-       // modelBuilder.Entity<Apuesta>()
-       //.HasOne(a => a.Ruleta)              
-       //.WithMany(r => r.Apuestas)        
-       //.HasForeignKey(a => a.IdRuleta)   
-       //.HasConstraintName("FK_Apuestas_Ruletas_IdRuleta"); 
     }
 }
